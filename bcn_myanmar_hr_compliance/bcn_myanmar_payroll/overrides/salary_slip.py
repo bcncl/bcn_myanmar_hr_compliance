9f7a2157eb4de7aa9987dcf8d4107fdcfd13b2fa
Wai Minn Aung <waiminn.bcn@gmail.com>
2025-02-11 15:13:49 +0630

    feat: initialize Salary Slip overrides

---
+++ bcn_myanmar_hr_compliance/bcn_myanmar_payroll/overrides/salary_slip.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, Business Centric Network Company Limited and Contributors
+# License: MIT. See license.txt
+
+import frappe
+from hrms.payroll.doctype.salary_slip.salary_slip import SalarySlip
+
+class BCNSalarySlip(SalarySlip):
+	pass
