c0bb4098cef5e1664b5b17e956eab9bf1436a97a
Chue Mi Mi Yadanar Htwe <chue.bcn@gmail.com>
2025-03-07 16:56:21 +0630

    feat: Tax Exemption Declaration & Proof Submission Validation

--- bcn_myanmar_hr_compliance/bcn_myanmar_payroll/overrides/employee_tax_exemption_declaration.py
+++ bcn_myanmar_hr_compliance/bcn_myanmar_payroll/overrides/employee_tax_exemption_declaration.py
@@ -10,7 +10,8 @@ from hrms.hr.utils import (
 	validate_tax_declaration,
 )
 from bcn_myanmar_hr_compliance.bcn_myanmar_payroll.utils import (
-	validate_duplicate_exemption_for_payroll_period
+	validate_duplicate_exemption_for_payroll_period,
+	validate_existing_salary_slip_of_selected_exemption_from_date
 )
 
 class BCNEmployeeTaxExemptionDeclaration(EmployeeTaxExemptionDeclaration):
@@ -20,6 +21,10 @@ class BCNEmployeeTaxExemptionDeclaration(EmployeeTaxExemptionDeclaration):
 		validate_duplicate_exemption_for_payroll_period(
 			self.doctype, self.name, self.payroll_period, self.custom_bcn_from_date, self.employee
 		)
+		validate_existing_salary_slip_of_selected_exemption_from_date(
+			self.custom_bcn_from_date, self.employee
+		)
+
 		self.set_total_declared_amount()
 		self.set_total_exemption_amount()
 		self.calculate_hra_exemption()
