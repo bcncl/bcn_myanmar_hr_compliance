145605ee70ae37e6478634a52590f61c7f75465e
Chue Mi Mi Yadanar Htwe <chue.bcn@gmail.com>
2025-03-07 17:06:43 +0630

    feat: BCN Myanmar HR Setting

---
+++ bcn_myanmar_hr_compliance/bcn_myanmar_payroll/doctype/bcn_myanmar_hr_settings/bcn_myanmar_hr_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Business Centric Network Company Limited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class BCNMyanmarHRSettings(Document):
+	pass
---
+++ bcn_myanmar_hr_compliance/bcn_myanmar_payroll/doctype/bcn_myanmar_hr_settings/test_bcn_myanmar_hr_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Business Centric Network Company Limited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBCNMyanmarHRSettings(FrappeTestCase):
+	pass
