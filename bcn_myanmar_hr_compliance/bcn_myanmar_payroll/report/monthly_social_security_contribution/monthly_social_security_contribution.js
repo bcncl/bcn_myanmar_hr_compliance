cdf8acdbe4e8bcc86deaed991d03ada97ba07346
Chue Mi Mi Yadanar Htwe <chue.bcn@gmail.com>
2025-05-02 11:10:46 +0630

    feat: Monthly Social Security Contribution Report

---
+++ bcn_myanmar_hr_compliance/bcn_myanmar_payroll/report/monthly_social_security_contribution/monthly_social_security_contribution.js
@@ -0,0 +1,48 @@
+// Copyright (c) 2025, Business Centric Network Company Limited and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Monthly Social Security Contribution"] = {
+	"filters": [
+		// {
+		// 	fieldname: "from_date",
+		// 	label: __("From"),
+		// 	fieldtype: "Date",
+		// 	default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+		// 	reqd: 1,
+		// 	width: "100px",
+		// },
+		{
+			fieldname: "upto_date",
+			label: __("Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1,
+			width: "100px",
+		},
+		{
+			fieldname: "currency",
+			fieldtype: "Link",
+			options: "Currency",
+			label: __("Currency"),
+			default: erpnext.get_currency(frappe.defaults.get_default("Company")),
+			width: "50px",
+			hidden: 1
+		},
+		{
+			fieldname: "employee",
+			label: __("Employee"),
+			fieldtype: "Link",
+			options: "Employee",
+			width: "100px",
+		},
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			width: "100px",
+			reqd: 1,
+		},
+	]
+};
