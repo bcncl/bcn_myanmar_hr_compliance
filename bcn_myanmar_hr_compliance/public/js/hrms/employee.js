fcec4dd46c86dbcac0e0991532fac625ac2c8f05
Chue Mi Mi Yadanar Htwe <chue.bcn@gmail.com>
2025-07-11 13:25:30 +0630

    feat: Change default value of Enable SSC on Employee and reset SSC Registration No and HSCISC values of employee when unchecked Enable SSC

---
+++ bcn_myanmar_hr_compliance/public/js/hrms/employee.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2025, Business Centric Network Company Limited and Contributors
+// License: MIT. See license.txt
+
+frappe.ui.form.on("Employee", {
+    refresh(frm) {
+
+    },
+    custom_bcn_enable_ssc(frm) {
+        if (frm.doc.custom_bcn_enable_ssc === 0) {
+            frm.set_value("custom_bcn_ssc_registration_no", null)
+            frm.set_value("custom_bcn_ssc_hscis_category", null)
+        }
+    }
+});
